2ad7a0f937781bee1d77f1179a5ad50cf992796f
Byron Anderson <byronanderson32@gmail.com>
2012-01-26 17:17:46 -0800

    Initial, empty import of project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FunForum::Application.config.secret_token = '66b921738dd5c381f006cd19c50d86cd3977d1a3d0a9a7c329f93dc26d4d8d3962a5e2a9a354e81e6bd88d86f1451cd8f82004be09054a67d069d3082b7284d7'
